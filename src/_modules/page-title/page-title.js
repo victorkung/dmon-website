aed2b9e108369e31fdc81f912d0d03f3fbff8c20
Victor Kung <victor.y.kung@gmail.com>
2016-03-28 20:02:23 -0500

    added page-title banner, testimonial module and created scaffolding for every page

---
+++ src/_modules/page-title/page-title.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var PageTitle = function() {
+  this.name = 'page-title';
+  console.log('%s module', this.name);
+};
+
+module.exports = PageTitle;
---
+++ src/_modules/testimonial/testimonial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var Testimonial = function() {
+  this.name = 'testimonial';
+  console.log('%s module', this.name);
+};
+
+module.exports = Testimonial;
