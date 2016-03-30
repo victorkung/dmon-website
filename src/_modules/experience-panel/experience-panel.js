fea66af242b39e06a4f80d5ec9b59d24420760e8
Victor Kung <victor.y.kung@gmail.com>
2016-03-30 16:41:27 -0500

    completed all pages and tested on mobile. just waiting on contact page

---
+++ src/_modules/experience-panel/experience-panel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var ExperiencePanel = function() {
+  this.name = 'experience-panel';
+  console.log('%s module', this.name);
+};
+
+module.exports = ExperiencePanel;
